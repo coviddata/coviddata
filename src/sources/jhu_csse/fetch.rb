73033acb8055e31c4c709e7d0adb549dd5ab748e
Brian Fosten <bfosten@users.noreply.github.com>
2020-04-02 10:28:11 -0700

    Always refetch all of the JHU source data

--- src/sources/jhu_csse/fetch.rb
+++ src/sources/jhu_csse/fetch.rb
@@ -20,11 +20,6 @@ module Sources
         formatted_date = date.strftime('%m-%d-%Y')
 
         file_path = "data/sources/jhu_csse/daily_reports/#{formatted_date}.csv"
-        file_exists = File.exist?(file_path)
-        if file_exists
-          puts "Data was already fetched: #{formatted_date}"
-          return true
-        end
 
         url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/#{formatted_date}.csv"
         begin
