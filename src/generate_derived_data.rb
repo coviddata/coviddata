1f7cfc047ac7aa37f566241200de53ed257fb9bd
Brian Fosten <bfosten@users.noreply.github.com>
2020-08-16 08:18:37 -0700

    Prevent the GitHub file size limit from being exceeded

--- src/generate_derived_data.rb
+++ src/generate_derived_data.rb
@@ -165,8 +165,17 @@ class GenerateDerivedData
   def write_json
     LOCATION_TYPES.each do |location_type|
       plural_name = LOCATION_TYPES_CONFIGS[location_type][:plural_name]
-      output_data = @location_types_location_keys_dates_data[location_type].map do |location_key, dates_data|
+      location_keys_dates_data = @location_types_location_keys_dates_data[location_type]
+      # The GitHub file size limit is 100MB, and places/stats_pretty.json can exceed this, so we'll limit
+      # the number of locations included here.
+      location_keys_dates_data = location_keys_dates_data.first(3000)
+      output_data = location_keys_dates_data.map do |location_key, dates_data|
         location = @location_types_location_keys_locations[location_type][location_key]
+        # The GitHub file size limit is 100MB, and places/stats_pretty.json can exceed this, so we'll limit
+        # the number of dates included here.
+        if location_type == :place
+          dates_data = Hash[dates_data.to_a.last(150)]
+        end
         location.delete(:location_type)
         {
           location_type => location,
